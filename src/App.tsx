966fae4dfb38de09e9c7b777d27c4426cca60487
Ranish <ranish.raj.shrestha@gmail.com>
2026-03-01 11:40:07 +0545

    vercle analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
+import { Analytics } from "@vercel/analytics/react"
 import "./App.css";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
-
