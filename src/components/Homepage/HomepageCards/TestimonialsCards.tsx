c3c68f76af15f911fd8dfac2b41b5236b54c400d
Ranish <ranish.raj.shrestha@gmail.com>
2026-03-02 11:07:15 +0545

    removed unused imports

--- src/components/Homepage/HomepageCards/TestimonialsCards.tsx
+++ src/components/Homepage/HomepageCards/TestimonialsCards.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export type Testimonial = {
   name: string;
   designation: string;
--- src/components/Homepage/VoicesOfCommunitySection.tsx
+++ src/components/Homepage/VoicesOfCommunitySection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import TestimonialsCards, {
   type Testimonial,
 } from "./HomepageCards/TestimonialsCards";
