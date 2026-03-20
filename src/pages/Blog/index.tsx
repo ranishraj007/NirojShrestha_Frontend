fab94d5acb07f945b34ccebf919901662e35950e
Ranish <ranish.raj.shrestha@gmail.com>
2026-03-20 15:34:10 +0545

    removed react from blog page

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import BlogStaticData from "@/components/constaintData/BlgoStaticData";
 import ArticleCard from "@/components/Homepage/HomepageCards/ArticleCard";
