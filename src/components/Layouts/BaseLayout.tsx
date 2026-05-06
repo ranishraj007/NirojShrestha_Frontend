90525b17377ed2bfbf901f5f6ed3accaba2878ed
Ranish <ranish.raj.shrestha@gmail.com>
2026-05-06 13:28:58 +0545

    added scroll to top function in the site

--- src/components/Layouts/BaseLayout.tsx
+++ src/components/Layouts/BaseLayout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 
 const BaseLayout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+
   return (
     <>
       <div className="flex min-h-screen flex-col bg-[#fbf7ee] text-[#2f2a22]">
